295ebaa113b568cfddfcc006a52dc8f8af7f2ec0
Jon Jackson <36890724+ocskier@users.noreply.github.com>
2023-03-24 22:15:54 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -31,9 +31,9 @@ app.use(
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
   console.log("YOU ARE IN THE PRODUCTION ENV");
-  app.use(express.static(path.join(__dirname, "../build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build/"));
+    res.sendFile(path.join(__dirname, "./client/build/"));
   });
 }
 
